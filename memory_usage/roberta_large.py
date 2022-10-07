cc07b49582f98138f1e8c13346ad9b78bd696d03
Feisi Fu <fufeisi@bu.edu>
2022-10-07 23:36:37 +0000

    Update memory analyze for roberta_large

--- memory_usage/roberta_large.py
+++ memory_usage/roberta_large.py
@@ -6,7 +6,7 @@ if torch.cuda.is_available():
     print('-'*20 + 'Using cuda' + '-'*20)
     roberta.cuda()
 roberta.train()
-print(f'Memory Peak after loading model: {torch.cuda.max_memory_allocated(device=None)/(10**6)} MB.')
+print(f'Memory after loading model: {torch.cuda.memory_allocated(device=None)/(10**6)} MB.')
 
 optimizer = torch.optim.Adam(roberta.parameters(), lr=0.0001)
 words = ['hello', 'world']
@@ -16,9 +16,9 @@ for word in words:
      tokens = roberta.encode('Hello world!')
      output = roberta.predict('mnli', tokens)
      loss = output.mean()
-     print(f'Memory Peak model+buffer: {torch.cuda.max_memory_allocated(device=None)/(10**6)} MB.')
+     print(f'Memory model+buffer: {torch.cuda.memory_allocated(device=None)/(10**6)} MB.')
      loss.backward()
-     print(f'Memory Peak model+gradient: {torch.cuda.max_memory_allocated(device=None)/(10**6)} MB.')
+     print(f'Memory model+gradient: {torch.cuda.memory_allocated(device=None)/(10**6)} MB.')
      optimizer.step()
-     print(f'Memory Peak model+gradient+optimizer: {torch.cuda.max_memory_allocated(device=None)/(10**6)} MB.')
+     print(f'Memory model+gradient+optimizer: {torch.cuda.memory_allocated(device=None)/(10**6)} MB.')
 
