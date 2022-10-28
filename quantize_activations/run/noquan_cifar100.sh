6f08eecbe6233eeeed1c781012d0bab6e0b39a4e
Feisi Fu <fufeisi@bu.edu>
2022-10-28 23:36:37 +0000

    fix bug

--- quantize_activations/run/noquan_cifar100.sh
+++ quantize_activations/run/noquan_cifar100.sh
@@ -1,4 +1,4 @@
 for size in 256 512
 do
-     python ../training.py --data_name CIFAR100 --model_name vgg19_bn --quant False --batch_size $size --epochs 200 --lr 0.05 --early_stop 90 100 --data_path ../../data --times 10
+     python ../training.py --data_name CIFAR100 --model_name vgg19_bn --quant 0 --batch_size $size --epochs 200 --early_stop 70 90 --data_path ../../data --times 10
 done
--- quantize_activations/wiki.py
+++ quantize_activations/wiki.py
@@ -1,4 +1,4 @@
-lr = {'MNIST': 0.001/128,'CIFAR10': 0.05/128, 'CIFAR100': 0.1/128}
+lr = {'MNIST': 0.001/128,'CIFAR10': 0.05/256, 'CIFAR100': 0.1/128}
 # milestones = {'MNIST': ,'CIFAR10': , 'CIFAR100': [60, 120, 160]}
 # momentum = {'MNIST': 0.9,'CIFAR10': 0.9, 'CIFAR100': 0.9}
 # weight_decay = {'MNIST': 5e-4,'CIFAR10': 5e-4, 'CIFAR100': 5e-4}
